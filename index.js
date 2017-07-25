6e48e06ba3588ced5b0a173abd7c1b72366d4f4b
matthewmcneilly <matthewmcneilly182@gmail.com>
2017-07-25 11:49:42 +0100

    Add: Events to send metadata from electron app to main window

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ This file is in charge of the main parent process
 */
 
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+
+// Allows mainWindow to be accessed across function calls
+let mainWindow;
 
 // See script section in packages.json to see where electron starts
 // It executes index.js
@@ -16,11 +20,18 @@ app.on('ready', () => {
   //console.log('Shes a lady, woow, woow woow, shes a lady');
 
   // Create a new window and load current directories index.html
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
 // When window recieves message from index.html execute call back function
-ipcMain.on('video:submit', () => {
+// Same event message can be used by different events
+// Event is useful to differeniate this
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    //console.log('Video Duration: ', metadata.format.duration);
 
+    // Send duration data to index.html
+    mainWindow.webContents.send('video:metadata', metadata.format.duration);
+  });
 });
